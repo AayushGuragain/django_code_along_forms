c66a57a0ca0f69d4a1ef57ee08a5cc46b57ba1e7
AayushGuragain <aayush.guragain@wmich.edu>
2021-12-08 21:04:14 -0500

    first transit edit draft two

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcacher = forms.CharField(required=False,
+                                widget=forms.HiddenInput)
+
+    def clean_botcacher(self):
+        botcacher = self.cleaned_data['botcacher']
+        if len(botcacher) >0:
+            raise forms.ValidationError("Gottcha Bot!")
+        return botcacher
--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
-from basicapp import forms #==> from basicapp import forms, the directory above
+from basicapp import forms #==> from . import forms, the directory above
 
 # Create your views here.
 def index(request):
     return render(request, 'basicapp/index.html')
 
 def form_name_view(request):
-    form_var = forms.FormName
+    form_variable = forms.FormName
 
     if request.method == 'POST':
-        form_var = forms.FormName(request.POST)
+        form_variable = forms.FormName(request.POST)
 
-        if form_var.is_valid():
-            #DO SOMETHING
+        if form_variable.is_valid():
+            #DO SOMETHING in console
             print("VALIDATION SUCCESS")
-            print("NAME: "+ form_var.cleaned_data['name'])
-            print("EMAIL: "+ form_var.cleaned_data['email'])
-            print("TEXT: "+ form_var.cleaned_data['text'])
+            print("NAME: "+ form_variable.cleaned_data['name'])
+            print("EMAIL: "+ form_variable.cleaned_data['email'])
+            print("TEXT: "+ form_variable.cleaned_data['text'])
 
-
-
-
-    form_dict = {'form_key': form_var}
+    form_dict = {'form_key': form_variable}
     return render(request, 'basicapp/form_page.html', context=form_dict)
